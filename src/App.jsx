f8836fe3bc176f4ea43cfede04d67f7afe265e85
vianey-jean <vianey.jean@ymail.com>
2024-01-14 19:05:41 +0400

    Mise a jour du code

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Tabs from "./components/Tabs"
+import PreviewButton from "./components/PreviewButton"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-zinc-900 min-h-screen flex flex-col">
+      <div className="px-4 py-4 border-b flex items-center">
+        <h1 className="w-full text-slate-50 text-2xl">
+          Editeur de  Code sur Navigateur
+        </h1>
+        <div className="w-full">
+          <PreviewButton />
+        </div>
+        <div className="w-full flex">
+          <div className="ml-auto mr-2">
+            <div className="w-[6px] h-[6px] bg-slate-200 rounded-full my-1"></div>
+            <div className="w-[6px] h-[6px] bg-slate-200 rounded-full my-1"></div>
+            <div className="w-[6px] h-[6px] bg-slate-200 rounded-full"></div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Tabs />
+    </div>
   )
 }
 
